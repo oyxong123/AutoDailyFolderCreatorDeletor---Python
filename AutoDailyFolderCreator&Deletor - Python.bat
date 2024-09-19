7e5520a5adaf5d64a6ebed96e15fe8534bbf4832
yuxuan <xuanyuong@gmail.com>
2024-09-19 11:16:04 +0800

    Change file names

--- AutoDailyFolderCreator&Deletor - Python.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Program Files\Python312\python.exe" "C:\Users\Admin\Documents\VS Code\Python\AutoDailyFolderCreator - Python\AutoDailyFolderCreator - Python.py"
-"C:\Program Files\Python312\python.exe" "C:\Users\Admin\Documents\VS Code\Python\AutoDailyFolderCreator - Python\AutoDailyFolderDeletor - Python.py"
---
+++ AutoDailyFolderCreatorDeletor - Python.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Program Files\Python312\python.exe" "C:\Users\Admin\Documents\VS Code\Python\AutoDailyFolderCreatorDeletor - Python\AutoDailyFolderCreator - Python.py"
+"C:\Program Files\Python312\python.exe" "C:\Users\Admin\Documents\VS Code\Python\AutoDailyFolderCreatorDeletor - Python\AutoDailyFolderDeletor - Python.py"
